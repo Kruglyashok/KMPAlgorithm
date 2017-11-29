4e39faad8b6a36a6c6dfe729f334dda2ed7c6fc5
TvoiSempai <lumpyourhead1@gmail.com>
2017-11-29 16:19:38 +0300

    added KMP

--- KMPAlgorithm/Main.cpp
+++ KMPAlgorithm/Main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <list>
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -26,19 +27,48 @@ void naiveSearch(string needle, string *haystack, int num, list< pair<int,int> >
 }
 
 //function for printing the elements in a list
-void showlist(list <pair <int,int> > g) {
+void showlist(list <pair <int,int> > l) {
 	list < pair <int,int> > ::iterator it;
-	for (it = g.begin(); it != g.end(); ++it)
+	for (it = l.begin(); it != l.end(); ++it)
 		cout << '\t' << "string : " << (*it).first << " pos : " << (*it).second << endl;
 	cout << '\n';
 }
 
-void kmp(string needle, string *haystack, int num, list < pair<int,int> > &list) {
+//prefix function
+vector<int> prefix(string needle) {
+	vector<int> v(needle.length());
+	v[0] = 0;
+	int j;
+	for (int i = 1; i < needle.length(); i++) {
+		j = v[i - 1];
+		while ((j > 0) && (needle[i] != needle[j]))	j = v[j - 1];         
+		if (needle[i] == needle[j]) j++;
+		v[i] = j;
+	}
+	return v;
+}
+//kmp search alg
+void kmpSearch(string needle, string *haystack, int num, list < pair<int,int> > &list) {
+	vector<int> pi(needle.length());
+	int k = 0;
+	pi = prefix(needle);
+	for (int m = 0; m < num; m++) {
+		for (int  i = 0; i < haystack[m].length(); ++i)
+		{
+			while ((k > 0) && (needle[k] != haystack[m][i]))
+				k = pi[k - 1];
 
+			if (needle[k] == haystack[m][i])
+				k++;
+
+			if (k == needle.length())
+			list.push_back(make_pair(m, i - needle.length() + 1));
+		}
+	}
 }
 
 int main(int argc, char **argv) {
-	list< pair<int, int> > naive;
+	list< pair<int, int> > naive, kmp;
 	string *haystack, needle; //haystack - source text, needle - string to find
 	ifstream fin(filename, ios_base::in);
 	if (fin.is_open()) {
@@ -46,14 +76,20 @@ int main(int argc, char **argv) {
 		cin >> needle;
 		int n = strNum(filename);
 		haystack = new string[n];
-		for (int i = 0; i < n; i++) getline(fin,haystack[i]);
+		for (int i = 0; i < n; i++) getline(fin, haystack[i]);
 		cout << "Text: " << endl;
 		cout << endl;
 		for (int i = 0; i < n; i++) cout << haystack[i] << "\n";
 		cout << endl;
 		naiveSearch(needle, haystack, n, naive);
-		cout << "Needle " << needle << " was found here:" << endl;
+		kmpSearch(needle, haystack, n, kmp);
+		cout << "Needle '" << needle << "' was found here with NAIVE search:" << endl;
 		showlist(naive);
+		cout << "Needle '" << needle << "' was found here with KMP search:" << endl;
+		showlist(kmp);
+		cout << endl;
+		if (naive == kmp) cout << "Eq" << endl;
+		else cout << "NotEq" << endl;
 		fin.close();
 	}
 	else{ 
